559a323f99feeb5225b30a4e21883fce9b2dc86d
aucub <78630225+aucub@users.noreply.github.com>
2023-06-28 19:23:38 +0800

    update

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -5,9 +5,7 @@
   # 您可以在此处导入其他的home-manager模块
   imports = [
     # 如果您想使用您自己的flake导出的模块，可以按下面的方式配置(from modules/home-manager):
-    outputs = [
-      alacritty btop fcitx5 fish helix htop neofetch ranger
-    ];
+    alacritty btop fcitx5 fish helix htop neofetch ranger
 
     # 或者使用其他flake导出的模块也可以按下面的方式配置(such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
