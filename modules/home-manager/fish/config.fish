11268c2248f1e3e08550c368e97557fb2ce2128c
aucub <78630225+aucub@users.noreply.github.com>
2023-07-01 22:41:39 +0800

    update

--- modules/home-manager/fish/config.fish
+++ modules/home-manager/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 function fish_greeting
- 
+
 end
 set -x EDITOR helix
 mcfly init fish | source
--- modules/home-manager/fish/default.nix
+++ modules/home-manager/fish/default.nix
@@ -2,8 +2,12 @@
 
   programs.fish = {
     enable = true;
+    interactiveShellInit = ''
+      set fish_greeting # Disable greeting
+      set -x EDITOR helix
+      mcfly init fish | source
+    '';
   };
-  home.file.".config/fish/config.fish".source = ./config.fish;
   home.file.".config/fish/fish_variables".source = ./fish_variables;
   home.file.".config/fish/functions".source = ./functions;
 }
