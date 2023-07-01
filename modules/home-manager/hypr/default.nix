689d1c1c1c0202bbdfa7681753f9667defd7994c
aucub <78630225+aucub@users.noreply.github.com>
2023-07-01 20:57:40 +0800

    update

--- modules/home-manager/hypr/default.nix
+++ modules/home-manager/hypr/default.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 {
   home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
+  home.file.".config/gtk-3.0".source = ./gtk-3.0;
   home.file.".config/mako" = {
     source = ./mako;
     recursive = true;
@@ -8,5 +9,5 @@
   home.file.".config/waybar".source = ./waybar;
   home.file.".config/wlogout".source = ./wlogout;
   home.file.".config/wofi".source = ./wofi;
-  home.file.".config/hypr/wallpapers/wallpaper.png".source = ../wallpapers/wallpaper.png;
+  home.file.".config/hypr/wallpapers/wallpaper.jpg".source = ./wallpaper.jpg;
 }
