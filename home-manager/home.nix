08b96ce577d0373a6418925ca35adfe20a4a5448
aucub <78630225+aucub@users.noreply.github.com>
2023-07-01 22:25:50 +0800

    update

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,7 +6,7 @@
   imports = [
     # 如果您想使用您自己的flake导出的模块，可以按下面的方式配置(from modules/home-manager):
     # outputs.homeManagerModules.example
-    ../modules/home-manager;
+    ../modules/home-manager
   
     # 或者使用其他flake导出的模块也可以按下面的方式配置(such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
