2949d90a2532fe7b0063194245f8f65aa3cae804
aucub <78630225+aucub@users.noreply.github.com>
2023-07-01 22:43:54 +0800

    update

--- modules/home-manager/fcitx5/default.nix
+++ modules/home-manager/fcitx5/default.nix
@@ -12,7 +12,7 @@
   i18n.inputMethod = {
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
-      fcitx5-configtool fcitx5-gtk fcitx5-qt fcitx5-chinese-addons fcitx5-table-extra
+      fcitx5-configtool fcitx5-gtk libsForQt5.fcitx5-qt fcitx5-chinese-addons fcitx5-table-extra
     ];
   };
 
