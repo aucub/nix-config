f9768ea4ed87d73e871c3cc14968bee75b40bb58
aucub <78630225+aucub@users.noreply.github.com>
2023-07-01 15:44:20 +0800

    update

--- modules/nixos/environment/variables.nix
+++ modules/nixos/environment/variables.nix
@@ -14,8 +14,15 @@
       QT_AUTO_SCREEN_SCALE_FACTOR = "1";
       CLUTTER_BACKEND = "wayland";
       __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      __GL_GSYNC_ALLOWED = "0";
+      __GL_VRR_ALLOWED = "0";
+      WLR_NO_HARDWARE_CURSORS = "1";
+      WLR_RENDERER_ALLOW_SOFTWARE = "1";
       LIBVA_DRIVER_NAME = "nvidia";
+      ANKI_WAYLAND = "1";
       LIBSEAT_BACKEND = "logind";
+      WLR_BACKEND = "vulkan";
+      WLR_DRM_NO_ATOMIC = "1";
       WLR_RENDERER = "vulkan";
       XDG_CURRENT_DESKTOP = "Hyprland";
       XDG_SESSION_DESKTOP = "Hyprland";
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -286,7 +286,17 @@
       };
     };
     services.NetworkManager-wait-online.enable = false;
-    services.seatd.enable = true;
+    services.seatd = {
+      enable = true;
+      description = "Seat management daemon";
+      script = "${pkgs.seatd}/bin/seatd -g wheel";
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        RestartSec = "1";
+      };
+      wantedBy = ["multi-user.target"];
+    };
     oomd = {
       enableRootSlice = true;
       enableUserServices = true;
