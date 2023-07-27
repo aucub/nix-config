f76a1373ed9a8b736f21e9e5bc5b11eb6ff511d3
aucub <78630225+aucub@users.noreply.github.com>
2023-07-28 04:48:06 +0800

    update

--- modules/nixos/hyprland/default.nix
+++ modules/nixos/hyprland/default.nix
@@ -24,33 +24,32 @@
     };
   };
 
-  environment.systemPackages = with pkgs; [
-    waybar # the status bar
-    swaybg # the wallpaper
-    hyprpaper
-    hyprland-protocols
-    swayidle # the idle timeout
-    swaylock # locking the screen
-    wlogout # logout menu
-    nwg-bar
-    wl-clipboard
-    wl-clipboard-x11
-    wlr-randr
-    wf-recorder # creen recording
-    grim # taking screenshots
-    slurp # selecting a region   to screenshot
-    wofi
-    mako # the notification   daemon, the same as dunst
-    swayosd
-    yad # a fork of zenity, for   creating dialogs
-    hyprpicker
-    swaylock-effects
-    pamixer
-    obs-studio-plugins.wlrobs
-    udiskie
-  ];
-
-  environment.systemPackages = [ nixpkgs-unstable.wl-clip-persist ];
+  environment.systemPackages = with pkgs;
+    [
+      waybar # the status bar
+      swaybg # the wallpaper
+      hyprpaper
+      hyprland-protocols
+      swayidle # the idle timeout
+      swaylock # locking the screen
+      wlogout # logout menu
+      nwg-bar
+      wl-clipboard
+      wl-clipboard-x11
+      wlr-randr
+      wf-recorder # creen recording
+      grim # taking screenshots
+      slurp # selecting a region to screenshot
+      wofi
+      mako # the notification daemon, the same as dunst
+      swayosd
+      yad # a fork of zenity, for creating dialogs
+      hyprpicker
+      swaylock-effects
+      pamixer
+      obs-studio-plugins.wlrobs
+      udiskie
+    ] ++ [ nixpkgs-unstable.wl-clip-persist ];
 
   systemd.user.targets.hyprland-session.wants =
     [ "xdg-desktop-autostart.target" ];
