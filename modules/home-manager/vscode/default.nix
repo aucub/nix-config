2cbd531acca7cd559a978069d1fb360700e9161d
aucub <78630225+aucub@users.noreply.github.com>
2023-07-27 14:58:20 +0800

    update

--- modules/home-manager/vscode/default.nix
+++ modules/home-manager/vscode/default.nix
@@ -1,5 +1,10 @@
-{ ... }:
+{ pkgs, config, lib, ... }:
 
 {
   home.file.".config/Code/User/settings.json".source = ./settings.json;
+  home.file.".config/Code/User/settings.json-bak".source = ./settings.json;
+  home.activation.removeExistingVSCodeProfile =
+    lib.hm.dag.entryBefore [ "checkLinkTargets" ] ''
+      rm -f "${config.xdg.configHome}/Code/User/settings.json"
+    '';
 }
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -190,7 +190,6 @@
     zsh = {
       enable = true;
       enableCompletion = true;
-      enableAutosuggestions = true;
       autosuggestions = {
         enable = true;
         strategy = [ "match_prev_cmd" ]; # one of "history", "match_prev_cmd"
