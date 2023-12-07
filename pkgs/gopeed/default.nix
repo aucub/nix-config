287e314d5ad362a23211fedfaa1235fe5de97d2a
aucub <78630225+aucub@users.noreply.github.com>
2023-12-08 05:11:19 +0800

    feat(chromium): add home-manager module for chromium browser with custom settings feat(default): import fcitx5, firefox, and chromium modules to default.nix feat(firefox): add home-manager module for firefox browser with custom settings and extensions feat(.github): add dependabot.yml and Nix.yml files to enable dependabot and nix flake checks refactor(home-manager): update home.nix file to add more programs and settings refactor(configuration.nix): simplify the flake input arguments and add comments feat(configuration.nix): add various settings for nix, boot, networking, time, fonts, xdg, sound, hardware, users, input method, shells, environment, programs, qt, services, and system state version update(flake.nix): change nixpkgs and home-manager inputs to use unstable channels feat(flake.nix): add NUR, home-manager and nix-alien inputs to access more packages and modules refactor(flake.nix): use variables for username and hostname to avoid hardcoding

---
+++ pkgs/gopeed/default.nix
@@ -0,0 +1,59 @@
+{ stdenv, fetchurl, dpkg, wrapGAppsHook, libappindicator, libayatana-indicator, libayatana-appindicator, libdbusmenu, ayatana-ido, libindicator, glibc, gtk3, lib, glib, libepoxy, at-spi2-core, cairo, gdk-pixbuf, harfbuzz, pango, udev, autoPatchelfHook, }:
+
+stdenv.mkDerivation rec {
+  pname = "gopeed";
+  version = "1.5.1";
+
+  src = fetchurl {
+    url = "https://github.com/GopeedLab/gopeed/releases/download/v${version}/Gopeed-v${version}-linux-amd64.deb";
+    hash = "sha256-5gy8a7QCcpGlLWqb8rd+A3PXSlN5f0CXacWOwf4lEfc=";
+  };
+
+  nativeBuildInputs = [ dpkg autoPatchelfHook ];
+
+  buildInputs = [
+    wrapGAppsHook
+    libappindicator
+    libayatana-indicator
+    libayatana-appindicator
+    libdbusmenu
+    ayatana-ido
+    libindicator
+    glibc
+    gtk3
+    lib
+    glib
+    libepoxy
+    at-spi2-core
+    cairo
+    gdk-pixbuf
+    harfbuzz
+    pango
+    udev
+  ];
+
+  unpackCmd = "dpkg-deb -x $curSrc source";
+
+  postPatch = ''
+    substituteInPlace $src/source/DEBIAN/control \
+      --replace "libayatana-appindicator3.so.1" "${libayatana-appindicator}/lib/libayatana-appindicator3.so.1"
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mv source/usr $out
+    substituteInPlace $out/share/applications/gopeed.desktop \
+      --replace "/usr" $out
+
+    runHook postInstall
+  '';
+
+  meta = with stdenv.lib; {
+    description = "Gopeed (full name Go Speed), a high-speed downloader developed by Golang + Flutter, supports (HTTP, BitTorrent, Magnet) protocol, and supports all platforms.";
+    homepage = "https://github.com/GopeedLab/gopeed";
+    license = licenses.gpl3;
+    platforms = platforms.linux;
+    maintainers = with maintainers; [aucub];
+  };
+}
