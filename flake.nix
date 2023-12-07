6ad256b72ac2494f16e68d51a46a9fbf6968e8c7
aucub <78630225+aucub@users.noreply.github.com>
2023-12-08 06:51:08 +0800

    feat(chromium): add home-manager module for chromium browser with custom settings feat(default): import fcitx5, firefox, and chromium modules to default.nix feat(firefox): add home-manager module for firefox browser with custom settings and extensions feat(.github): add dependabot.yml and Nix.yml files to enable dependabot and nix flake checks refactor(home-manager): update home.nix file to add more programs and settings refactor(configuration.nix): simplify the flake input arguments and add comments feat(configuration.nix): add various settings for nix, boot, networking, time, fonts, xdg, sound, hardware, users, input method, shells, environment, programs, qt, services, and system state version update(flake.nix): change nixpkgs and home-manager inputs to use unstable channels feat(flake.nix): add NUR, home-manager and nix-alien inputs to access more packages and modules refactor(flake.nix): use variables for username and hostname to avoid hardcoding

--- flake.nix
+++ flake.nix
@@ -1,16 +1,17 @@
 {
-  description = "Your new nix config";
+  description = "nix config";
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/master";
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
+    nur.url = "github:nix-community/NUR";
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # TODO: Add any other flake you might need
@@ -19,12 +20,17 @@
     # Shameless plug: looking for a way to nixify your themes and make
     # everything match nicely? Try nix-colors!
     # nix-colors.url = "github:misterio77/nix-colors";
+
+    nix-alien.url = "github:thiagokokada/nix-alien";
   };
 
   outputs = {
     self,
     nixpkgs,
+    nixpkgs-unstable,
     home-manager,
+    nur,
+    nix-alien,
     ...
   } @ inputs: let
     inherit (self) outputs;
@@ -42,10 +48,13 @@
   in {
     # Your custom packages
     # Accessible through 'nix build', 'nix shell', etc
-    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+    packages =
+      forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
+
     # Formatter for your nix files, available through 'nix fmt'
     # Other options beside 'alejandra' include 'nixpkgs-fmt'
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+    formatter =
+      forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
     # Your custom packages and modifications, exported as overlays
     overlays = import ./overlays {inherit inputs;};
@@ -60,11 +69,33 @@
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
       # FIXME replace with your hostname
-      your-hostname = nixpkgs.lib.nixosSystem {
+      "your-hostname" = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [
           # > Our main nixos configuration file <
           ./nixos/configuration.nix
+          nur.nixosModules.nur
+          ({config, ...}: {
+            # NUR
+            environment.systemPackages = with config.nur.repos; [
+              # linyinfeng.wemeet
+              # xddxdd.dingtalk
+              # rewine.ttf-wps-fonts
+              # rewine.ttf-ms-win10
+              ruixi-rebirth.fcitx5-pinyin-moegirl
+              ruixi-rebirth.fcitx5-pinyin-zhwiki
+            ];
+          })
+          # nix-alien
+          ({
+            self,
+            system,
+            ...
+          }: {
+            environment.systemPackages = with self.inputs.nix-alien.packages.${system}; [nix-alien];
+            # Optional, needed for `nix-alien-ld`
+            # programs.nix-ld.enable = true;
+          })
         ];
       };
     };
@@ -74,7 +105,8 @@
     homeConfigurations = {
       # FIXME replace with your username@hostname
       "your-username@your-hostname" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         modules = [
           # > Our main home-manager configuration file <
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,10 +18,14 @@
     # inputs.hardware.nixosModules.common-ssd
 
     # You can also split up your configuration and import pieces of it here:
+    # disable nvidia
+    # inputs.nixos-hardware.nixosModules.common-gpu-nvidia-disable
     # ./users.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
+    # Import home-manager's NixOS module
+    # inputs.home-manager.nixosModules.home-manager
   ];
 
   nixpkgs = {
@@ -51,38 +55,172 @@
 
   # This will add each flake input as a registry
   # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+  nix.registry =
+    (lib.mapAttrs (_: flake: {inherit flake;}))
+    ((lib.filterAttrs (_: lib.isType "flake")) inputs);
 
   # This will additionally add your inputs to the system's legacy channels
   # Making legacy nix commands consistent as well, awesome!
   nix.nixPath = ["/etc/nix/path"];
   environment.etc =
-    lib.mapAttrs'
-    (name: value: {
+    lib.mapAttrs' (name: value: {
       name = "nix/path/${name}";
       value.source = value.flake;
     })
     config.nix.registry;
 
   nix.settings = {
-    # Enable flakes and new 'nix' command
-    experimental-features = "nix-command flakes";
     # Deduplicate and optimize nix store
     auto-optimise-store = true;
+    substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://mirrors.bfsu.edu.cn/nix-channels/store"
+      "https://cache.nixos.org/"
+            "https://qihaiumi.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
+    ];
+    trusted-public-keys = [
+      "qihaiumi.cachix.org-1:0Bt2C1k4MRhqyeUVCOIQJ4O87Wvn5mR0QN7GaNa5AnU="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+    ];
+    trusted-users = ["root" "@wheel"];
   };
 
+nix.gc = {
+      automatic = true;
+      dates = "weekly";
+    };
   # FIXME: Add the rest of your current configuration
 
   # TODO: Set your hostname
   networking.hostName = "your-hostname";
 
   # TODO: This is just an example, be sure to use whatever bootloader you prefer
-  boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.enable = true;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_lqx;
+    loader = {
+      systemd-boot = {
+        enable = true;
+        graceful = true;
+        configurationLimit = 5;
+      };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+    };
+    kernelParams = [
+      "amd_pstate=passive"
+      "amdgpu.vm_update_mode=3"
+      "radeon.dpm=0"
+      "nvidia-drm.modeset=1"
+      "acpi_backlight=native"
+      "NVreg_PreserveVideoMemoryAllocations=1"
+    ];
+    consoleLogLevel = 3;
+    initrd.kernelModules = ["btrfs"];
+    kernelModules = [
+      "v4l2loopback"
+      "bbswitch"
+      "amdgpu"
+      "nvidia"
+      "nvidia_drm"
+      "nvidia_uvm"
+      "nvidia_modeset"
+    ];
+    extraModulePackages = [
+      pkgs.linuxKernel.packages.linux_lqx.bbswitch
+      pkgs.linuxKernel.packages.linux_lqx.v4l2loopback
+    ];
+    extraModprobeConfig = ''
+      options v4l2loopback exclusive_caps=1 video_nr=9 card_label="obs"
+    '';
+    tmp.useTmpfs = true;
+  };
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Asia/Shanghai";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales = ["zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8"];
+
+  fonts = {
+    enableDefaultPackages = true;
+    fontDir.enable = true;
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-emoji
+      sarasa-gothic
+      source-han-serif
+      source-han-sans
+      source-han-mono
+      source-han-serif-vf-otf
+      source-han-sans-vf-otf
+      twitter-color-emoji
+    ];
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = ["Noto Serif CJK SC"];
+        sansSerif = ["Sarasa UI SC"];
+        monospace = ["Sarasa Mono SC"];
+        emoji = ["Twemoji" "Noto Color Emoji"];
+      };
+    };
+  };
+
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    extraPortals = with pkgs; [];
+  };
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  hardware = {
+    nvidia = {
+      package = pkgs.linuxKernel.packages.linux_lqx.nvidia_x11_vulkan_beta_open;
+      powerManagement.enable = true;
+      powerManagement.finegrained = true;
+      open = true;
+      nvidiaSettings = true;
+      modesetting.enable = true;
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+      };
+    };
+    opengl = {
+      enable = true;
+      driSupport = true;
+      extraPackages = with pkgs; [
+        amdvlk
+        vaapiVdpau
+        nvidia-vaapi-driver
+        libvdpau-va-gl
+      ];
+    };
+    brillo.enable = true;
+    bluetooth.enable = true;
+    # disable nvidia
+    nvidiaOptimus.disable = false;
+  };
+
+  users.users.root.initialPassword = "root";
+  users.users.root.shell = pkgs.bash;
 
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
     # FIXME: Replace with your username
-    your-username = {
+    "your-username" = {
       # TODO: You can set an initial password for your user.
       # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
       # Be sure to change it (using passwd) after rebooting!
@@ -92,14 +230,152 @@
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["wheel"];
+      extraGroups = ["wheel" "networkmanager"];
+      shell = pkgs.bash;
+      packages = with pkgs; [
+        inputs.home-manager.packages.${pkgs.system}.default
+        vscode
+        firefox
+        google-chrome
+        obs-studio
+        localsend
+        (python311.withPackages (ps: with ps; [selenium black]))
+        # postman
+        # jetbrains.idea-ultimate
+        # bun
+        # qq
+      ];
     };
+  };
+
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [fcitx5-with-addons fcitx5-chinese-addons];
+  };
+
+  environment.shells = with pkgs; [bashInteractive fish];
+
+  environment.variables = {
+    EDITOR = "helix";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+    SDL_IM_MODULE = "fcitx";
+    GLFW_IM_MODULE = "ibus";
+  };
+
+  environment.sessionVariables = {
+  };
+
+  environment.systemPackages = with pkgs; [
+    inputs.home-manager.packages.${pkgs.system}.default
+    helix
+    git
+    eza
+    yazi
+    fzf
+    bat
+    fd
+    ripgrep-all
+    mcfly
+    htop
+    linux-wifi-hotspot
+    python311
+    orchis-theme
+  ];
+
+  documentation = {
+    enable = false;
+    doc.enable = false;
+  };
+
+  programs = {
+    command-not-found.enable = false;
+    xwayland.enable = true;
+    fish = {
+      enable = true;
+      useBabelfish = true;
+      shellInit = ''set -U fish_greeting " "'';
+      interactiveShellInit = ''
+        function rga-fzf
+            set RG_PREFIX 'rga --files-with-matches'
+            if test (count $argv) -gt 1
+                set RG_PREFIX "$RG_PREFIX $argv[1..-2]"
+            end
+            set -l file $file
+            set file (
+            FZF_DEFAULT_COMMAND="$RG_PREFIX '$argv[-1]'" \
+            fzf --sort \
+                --preview='test ! -z {} && \
+                    rga --pretty --context 5 {q} {}' \
+                --phony -q "$argv[-1]" \
+                --bind "change:reload:$RG_PREFIX {q}" \
+                --preview-window='50%:wrap'
+        ) && echo "opening $file" && open "$file"
+        end
+      '';
+    };
+    fzf.fuzzyCompletion = true;
+    firefox = {languagePacks = ["zh-CN"];};
+    yazi.settings.yazi = {manager.show_hidden = true;};
+  };
+
+  qt = {
+    enable = true;
+  };
+
+  services = {
+    auto-cpufreq.enable = true;
+    btrfs.autoScrub = {
+      enable = true;
+      interval = "monthly";
+      fileSystems = ["/"];
+    };
+    pipewire = {
+      enable = true;
+      audio.enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      jack.enable = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
+    };
+    xserver = {
+      enable = true;
+      videoDrivers = ["amdgpu"];
+      displayManager = {
+        gdm = {enable = true;};
+        autoLogin = {
+          enable = true;
+          user = "your-username";
+        };
+      };
+      desktopManager = {
+        xterm.enable = false;
+        gnome = {
+          enable = true;
+        };
+      };
+      excludePackages = with pkgs; [
+      ];
+      libinput = {
+        enable = true;
+        mouse = {accelProfile = "adaptive";};
+        touchpad = {
+          tapping = true;
+          naturalScrolling = true;
+          accelProfile = "adaptive";
+          disableWhileTyping = true;
+        };
+      };
+    };
+    # CUPS
+    # printing.enable = true;
   };
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
-    enable = true;
+    enable = false;
     settings = {
       # Forbid root login through SSH.
       PermitRootLogin = "no";
@@ -109,5 +385,5 @@
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "23.05";
+  system.stateVersion = "24.05";
 }
