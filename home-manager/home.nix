07fd2de7ee993cc5c82226d1318449c45bed0aac
aucub <78630225+aucub@users.noreply.github.com>
2023-12-08 06:51:08 +0800

    feat(chromium): add home-manager module for chromium browser with custom settings feat(default): import fcitx5, firefox, and chromium modules to default.nix feat(firefox): add home-manager module for firefox browser with custom settings and extensions feat(.github): add dependabot.yml and Nix.yml files to enable dependabot and nix flake checks refactor(home-manager): update home.nix file to add more programs and settings refactor(configuration.nix): simplify the flake input arguments and add comments feat(configuration.nix): add various settings for nix, boot, networking, time, fonts, xdg, sound, hardware, users, input method, shells, environment, programs, qt, services, and system state version update(flake.nix): change nixpkgs and home-manager inputs to use unstable channels feat(flake.nix): add NUR, home-manager and nix-alien inputs to access more packages and modules refactor(flake.nix): use variables for username and hostname to avoid hardcoding

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -12,6 +12,10 @@
   imports = [
     # If you want to use modules your own flake exports (from modules/home-manager):
     # outputs.homeManagerModules.example
+    # outputs.homeManagerModules.fcitx5
+    # outputs.homeManagerModules.fastfetch
+    # outputs.homeManagerModules.firefox
+    # outputs.homeManagerModules.chromium
 
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
@@ -42,15 +46,14 @@
     config = {
       # Disable if you don't want unfree packages
       allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
     };
   };
 
   # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "${vars.username}";
+    homeDirectory = "/home/${vars.username}";
+    language.base = "zh_CN.UTF-8";
   };
 
   # Add stuff for your user as you see fit:
@@ -58,12 +61,381 @@
   # home.packages = with pkgs; [ steam ];
 
   # Enable home-manager and git
-  programs.home-manager.enable = true;
-  programs.git.enable = true;
+  # programs.home-manager.enable = true;
+  # programs.git.enable = true;
+
+  programs = {
+    vscode = {
+      enable = true;
+      enableExtensionUpdateCheck = false;
+      enableUpdateCheck = false;
+      extensions = with pkgs.vscode-extensions; [
+        # ms-vscode.cpptools
+        ms-ceintl.vscode-language-pack-zh-hans
+        formulahendry.code-runner
+        mhutchie.git-graph
+        oderwat.indent-rainbow
+        # rust-lang.rust-analyzer
+        redhat.vscode-yaml
+        redhat.vscode-xml
+        yzhang.markdown-all-in-one
+        ms-python.python
+        ms-python.vscode-pylance
+        # ms-python.black-formatter
+        foxundermoon.shell-format
+        tamasfe.even-better-toml
+        bmalehorn.vscode-fish
+      ];
+      userSettings = {
+        "extensions.ignoreRecommendations" = true;
+        "window.dialogStyle" = "custom";
+        "window.titleBarStyle" = "custom";
+        "editor.cursorSmoothCaretAnimation" = "on";
+        "editor.fontFamily" = ''"Sarasa Mono SC",monospace'';
+        "editor.fontLigatures" = true;
+        "editor.fontSize" = 18;
+        "editor.suggestSelection" = "first";
+        "files.exclude" = {
+          "**/.classpath" = true;
+          "**/.factorypath" = true;
+          "**/.project" = true;
+          "**/.settings" = true;
+          "**/.vscode" = true;
+          "**/**.exe" = true;
+        };
+        "workbench.startupEditor" = "none";
+        "grunt.autoDetect" = "on";
+        "security.workspace.trust.untrustedFiles" = "open";
+        "editor.inlineSuggest.enabled" = true;
+        "emmet.syntaxProfiles" = {
+          "vue-html" = "html";
+          "vue" = "html";
+        };
+        "editor.acceptSuggestionOnCommitCharacter" = false;
+        "telemetry.telemetryLevel" = "off";
+        "update.showReleaseNotes" = false;
+        "editor.wordWrap" = "on";
+        "code-runner.runInTerminal" = true;
+        "files.autoSave" = "onWindowChange";
+        "indentRainbow.colors" = [
+          "rgba(255,255,64,0.07)"
+          "rgba(127,255,127,0.07)"
+          "rgba(255,127,255,0.07)"
+          "rgba(79,236,236,0.07)"
+        ];
+        "git.autofetch" = true;
+        "git.confirmSync" = false;
+        "explorer.confirmDelete" = false;
+        "[json]" = {
+          "editor.defaultFormatter" = "vscode.json-language-features";
+        };
+        "[markdown]" = {
+          "editor.defaultFormatter" = "yzhang.markdown-all-in-one";
+        };
+        "[typescript]" = {
+          "editor.defaultFormatter" = "vscode.typescript-language-features";
+        };
+        "workbench.enableExperiments" = false;
+        "window.autoDetectColorScheme" = true;
+        "terminal.integrated.defaultProfile.linux" = "fish";
+        "update.mode" = "none";
+        "editor.codeLensFontFamily" = ''"Sarasa Mono SC",monospace'';
+        "github.gitProtocol" = "ssh";
+        "[python]" = {"editor.defaultFormatter" = "mikoz.black-py";};
+        "editor.accessibilitySupport" = "off";
+        "window.commandCenter" = false;
+        "code-runner.showExecutionMessage" = false;
+        "diffEditor.ignoreTrimWhitespace" = true;
+        "redhat.telemetry.enabled" = false;
+        "bitoAI.codeCompletion.enableAutoCompletion" = true;
+        "bitoAI.codeCompletion.enableCommentToCode" = true;
+        "workbench.colorTheme" = "Quiet Light";
+        "code-runner.enableAppInsights" = false;
+        "editor.inlineSuggest.suppressSuggestions" = true;
+      };
+    };
+    # 启用 home-manager and git
+    home-manager.enable = true;
+    # git 相关配置
+    git = {
+      enable = true;
+      userName = "aucub";
+      userEmail = "78630225+aucub@users.noreply.github.com";
+      ignores = [
+        ".cache"
+        ".DS_Store"
+        ".idea"
+        ".fastRequest"
+        "node_modules"
+        ".vscode"
+        ".gradle"
+        "build"
+        "!**/src/main/**/build"
+        "!**/src/test/**/build"
+        "*.iws"
+        "*.iml"
+        "*.ipr"
+        "*.log"
+        "out"
+        "!**/src/main/**/out"
+        "!**/src/test/**/out"
+        ".venv"
+      ];
+      extraConfig = {
+        init.defaultBranch = "main";
+        core.editor = "helix";
+        core.autocrlf = "input";
+        push.autoSetupRemote = true;
+        core.pager = "bat";
+        diff.tool = "difftastic";
+        difftool.prompt = false;
+        difftool.difftastic.cmd = ''difft "$LOCAL" "$REMOTE"'';
+        pager.difftool = true;
+      };
+    };
+    eza = {
+      enable = true;
+      git = true;
+      extraOptions = ["--group-directories-first" "--all"];
+    };
+    mcfly = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+    fish.functions = {
+        rga-fzf = ''
+              set RG_PREFIX 'rga --files-with-matches'
+              if test (count $argv) -gt 1
+                  set RG_PREFIX "$RG_PREFIX $argv[1..-2]"
+              end
+              set -l file $file
+              set file (
+              FZF_DEFAULT_COMMAND="$RG_PREFIX '$argv[-1]'" \
+              fzf --sort \
+                  --preview='test ! -z {} && \
+                      rga --pretty --context 5 {q} {}' \
+                  --phony -q "$argv[-1]" \
+                  --bind "change:reload:$RG_PREFIX {q}" \
+                  --preview-window='50%:wrap'
+          ) && echo "opening $file" && open "$file"
+          end
+        '';
+      };
+    htop = {
+      enable = true;
+      settings = {
+        hide_kernel_threads = 1;
+        hide_userland_threads = 1;
+        show_program_path = 0;
+        highlight_base_name = 1;
+        show_cpu_frequency = 1;
+        show_cpu_temperature = 1;
+        color_scheme = 6;
+      };
+    };
+    helix = {
+      enable = true;
+      defaultEditor = true;
+      settings = {
+        theme = "github_dark_colorblind";
+        editor = {
+          middle-click-paste = false;
+          file-picker.hidden = false;
+        };
+      };
+    };
+    bat = {
+      enable = true;
+      config = {
+        style = "header-filename,header-filesize,grid";
+        paging = "never";
+        theme = "Dracula";
+      };
+    };
+    fzf = {
+      enable = true;
+      fuzzyCompletion = true;
+      defaultCommand = "rga --files || fd || find .";
+      defaultOptions = [
+        "--preview='bat {} -n --color=always'"
+        "--preview-window='right,50%,wrap,border-none'"
+        "--border=none"
+        "--layout=reverse"
+        "--bind 'enter:become(helix {})'"
+      ];
+      colors = {
+        "bg+" = "#313244";
+        "bg" = "#1e1e2e";
+        "spinner" = "#f5e0dc";
+        "hl" = "#f38ba8";
+        "fg" = "#cdd6f4";
+        "header" = "#f38ba8";
+        "info" = "#cba6f7";
+        "pointer" = "#f5e0dc";
+        "marker" = "#f5e0dc";
+        "fg+" = "#cdd6f4";
+        "prompt" = "#cba6f7";
+        "hl+" = "#f38ba8";
+      };
+    };
+    obs-studio = {
+      enable = true;
+      plugins = with pkgs.obs-studio-plugins; [
+        obs-pipewire-audio-capture
+        obs-scale-to-sound
+        obs-vaapi
+        advanced-scene-switcher
+      ];
+    };
+  };
+  dconf.settings = {
+        "org/gnome/shell" = {
+          favorite-apps = [
+            "org.gnome.settings.desktop"
+            "alacritty.desktop"
+            "firefox.desktop"
+            "emacs.desktop"
+            "org.gnome.nautilus.desktop"
+            "com.obsproject.studio.desktop"
+            "plexmediaplayer.desktop"
+            "smartcode-stremio.desktop"
+            "discord.desktop"
+            "steam.desktop"
+            "retroarch.desktop"
+            "com.parsecgaming.parsec.desktop"
+            "org.remmina.remmina.desktop"
+            "virt-manager.desktop"
+            # "blueman-manager.desktop"
+            # "pavucontrol.desktop"
+          ];
+              "org/gnome/nautilus/icon-view" = {
+      default-zoom-level = "standard";
+    };
+
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+
+    "org/gnome/nautilus/preferences" = {
+      default-folder-viewer = "icon-view";
+      default-sort-order = "type";
+      search-filter-time-type = "last_modified";
+      search-view = "list-view";
+    };
+        "org/gnome/TextEditor" = {
+      custom-font = "FiraCode Nerd Font Mono Medium 14";
+      highlight-current-line = true;
+      indent-style = "space";
+      show-line-numbers = true;
+      show-map = true;
+      show-right-margin = true;
+      style-scheme = "builder-dark";
+      tab-width = mkUint32 4;
+      use-system-font = false;
+    };
+          disable-user-extensions = false;
+          enabled-extensions = [
+            "trayiconsreloaded@selfmade.pl"
+            "blur-my-shell@aunetx"
+            "drive-menu@gnome-shell-extensions.gcampax.github.com"
+            "dash-to-panel@jderose9.github.com"
+            "just-perfection-desktop@just-perfection"
+            "caffeine@patapon.info"
+            "clipboard-indicator@tudmotu.com"
+            "horizontal-workspace-indicator@tty2.io"
+            "bluetooth-quick-connect@bjarosze.gmail.com"
+            "battery-indicator@jgotti.org"
+            "gsconnect@andyholmes.github.io"
+            "pip-on-top@rafostar.github.com"
+            "forge@jmmaranan.com"
+            # "dash-to-dock@micxgx.gmail.com"           # Alternative Dash-to-Panel
+            # "fullscreen-avoider@noobsai.github.com"   # Dash-to-Panel Incompatable
+          ];
+        };
+
+        "org/gnome/desktop/interface" = {
+          color-scheme = "prefer-dark";
+          enable-hot-corners = false;
+          clock-show-weekday = true;
+          # gtk-theme = "adwaita-dark";
+        };
+        # "org/gnome/desktop/session" = {               # Not Working
+        #   idle-delay = "uint32 900";
+        # };
+        "org/gnome/desktop/privacy" = {
+          report-technical-problems = "false";
+        };
+        "org/gnome/desktop/calendar" = {
+          show-weekdate = true;
+        };
+        "org/gnome/desktop/wm/preferences" = {
+          action-right-click-titlebar = "toggle-maximize";
+          action-middle-click-titlebar = "minimize";
+          resize-with-right-button = true;
+          mouse-button-modifier = "<super>";
+          button-layout = ":minimize,close";
+        };
+        "org/gnome/desktop/wm/keybindings" = {
+          # maximize = ["<super>up"];                   # Floating
+          # unmaximize = ["<super>down"];
+          maximize = ["@as []"];                        # Tiling
+          unmaximize = ["@as []"];
+          switch-to-workspace-left = ["<alt>left"];
+          switch-to-workspace-right = ["<alt>right"];
+          switch-to-workspace-1 = ["<alt>1"];
+          switch-to-workspace-2 = ["<alt>2"];
+          switch-to-workspace-3 = ["<alt>3"];
+          switch-to-workspace-4 = ["<alt>4"];
+          switch-to-workspace-5 = ["<alt>5"];
+          move-to-workspace-left = ["<shift><alt>left"];
+          move-to-workspace-right = ["<shift><alt>right"];
+          move-to-workspace-1 = ["<shift><alt>1"];
+          move-to-workspace-2 = ["<shift><alt>2"];
+          move-to-workspace-3 = ["<shift><alt>3"];
+          move-to-workspace-4 = ["<shift><alt>4"];
+          move-to-workspace-5 = ["<shift><alt>5"];
+          move-to-monitor-left = ["<super><alt>left"];
+          move-to-monitor-right = ["<super><alt>right"];
+          close = ["<super>q" "<alt>f4"];
+          toggle-fullscreen = ["<super>f"];
+        };
+
+        "org/gnome/mutter" = {
+          workspaces-only-on-primary = false;
+          center-new-windows = true;
+          edge-tiling = false;                          # Tiling
+        };
+        "org/gnome/mutter/keybindings" = {
+          #toggle-tiled-left = ["<super>left"];         # Floating
+          #toggle-tiled-right = ["<super>right"];
+          toggle-tiled-left = ["@as []"];               # Tiling
+          toggle-tiled-right = ["@as []"];
+        };
+        "org/gnome/settings-daemon/plugins/power" = {
+          sleep-interactive-ac-type = "nothing";
+        };
+        "org/gnome/shell/extensions/caffeine" = {
+          enable-fullscreen = true;
+          restore-state = true;
+          show-indicator = true;
+          show-notification = false;
+        };
+      };
+
+ services = {
+        udiskie = {
+          enable = true;
+          automount = true;
+          tray = "auto";
+        };
+      };
+
+  # notifications about home-manager news
+  news.display = "silent";
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "23.05";
+  home.stateVersion = "24.05";
 }
--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,13 +1,6 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
-{
-  inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
-  ...
-}: {
+{ inputs, outputs, lib, config, pkgs, vars, ... }: {
   # You can import other NixOS modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/nixos):
@@ -18,10 +11,14 @@
     # inputs.hardware.nixosModules.common-ssd
 
     # You can also split up your configuration and import pieces of it here:
+    # disable nvidia
+    # inputs.nixos-hardware.nixosModules.common-gpu-nvidia-disable
     # ./users.nix
 
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
+    # Import home-manager's NixOS module
+    # inputs.home-manager.nixosModules.home-manager
   ];
 
   nixpkgs = {
@@ -51,38 +48,170 @@
 
   # This will add each flake input as a registry
   # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+  nix.registry = (lib.mapAttrs (_: flake: { inherit flake; }))
+    ((lib.filterAttrs (_: lib.isType "flake")) inputs);
 
   # This will additionally add your inputs to the system's legacy channels
   # Making legacy nix commands consistent as well, awesome!
-  nix.nixPath = ["/etc/nix/path"];
-  environment.etc =
-    lib.mapAttrs'
-    (name: value: {
-      name = "nix/path/${name}";
-      value.source = value.flake;
-    })
-    config.nix.registry;
+  nix.nixPath = [ "/etc/nix/path" ];
+  environment.etc = lib.mapAttrs' (name: value: {
+    name = "nix/path/${name}";
+    value.source = value.flake;
+  }) config.nix.registry;
 
   nix.settings = {
-    # Enable flakes and new 'nix' command
-    experimental-features = "nix-command flakes";
     # Deduplicate and optimize nix store
     auto-optimise-store = true;
+    substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://mirrors.bfsu.edu.cn/nix-channels/store"
+      "https://cache.nixos.org/"
+      "https://qihaiumi.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
+    ];
+    trusted-public-keys = [
+      "qihaiumi.cachix.org-1:0Bt2C1k4MRhqyeUVCOIQJ4O87Wvn5mR0QN7GaNa5AnU="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+    ];
+    trusted-users = [ "root" "@wheel" ];
   };
 
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 2d";
+  };
   # FIXME: Add the rest of your current configuration
 
   # TODO: Set your hostname
-  networking.hostName = "your-hostname";
+  networking.hostName = "${vars.hostname}";
 
   # TODO: This is just an example, be sure to use whatever bootloader you prefer
-  boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.enable = true;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_lqx;
+    loader = {
+      systemd-boot = {
+        enable = true;
+        graceful = true;
+        configurationLimit = 5;
+      };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+    };
+    kernelParams = [
+      "amd_pstate=passive"
+      "amdgpu.vm_update_mode=3"
+      "radeon.dpm=0"
+      "nvidia-drm.modeset=1"
+      "acpi_backlight=native"
+      "NVreg_PreserveVideoMemoryAllocations=1"
+    ];
+    consoleLogLevel = 3;
+    initrd.kernelModules = [ "btrfs" ];
+    kernelModules = [
+      "v4l2loopback"
+      "bbswitch"
+      "amdgpu"
+      "nvidia"
+      "nvidia_drm"
+      "nvidia_uvm"
+      "nvidia_modeset"
+    ];
+    extraModulePackages = [
+      pkgs.linuxKernel.packages.linux_lqx.bbswitch
+      pkgs.linuxKernel.packages.linux_lqx.v4l2loopback
+    ];
+    extraModprobeConfig = ''
+      options v4l2loopback exclusive_caps=1 video_nr=9 card_label="Virtual Camera"
+    '';
+    tmp.useTmpfs = true;
+  };
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Asia/Shanghai";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
+
+  fonts = {
+    enableDefaultPackages = true;
+    fontDir.enable = true;
+    packages = with pkgs; [
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      noto-fonts-emoji
+      sarasa-gothic
+      source-han-serif
+      source-han-sans
+      source-han-mono
+      source-han-serif-vf-otf
+      source-han-sans-vf-otf
+      twitter-color-emoji
+    ];
+    fontconfig = {
+      enable = true;
+      defaultFonts = {
+        serif = [ "Noto Serif CJK SC" ];
+        sansSerif = [ "Sarasa UI SC" ];
+        monospace = [ "Sarasa Mono SC" ];
+        emoji = [ "Twemoji" "Noto Color Emoji" ];
+      };
+    };
+  };
+
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    extraPortals = with pkgs; [ ];
+  };
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  hardware = {
+    nvidia = {
+      package = pkgs.linuxKernel.packages.linux_lqx.nvidia_x11_vulkan_beta_open;
+      powerManagement.enable = true;
+      powerManagement.finegrained = true;
+      open = true;
+      nvidiaSettings = true;
+      modesetting.enable = true;
+      prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+      };
+    };
+    opengl = {
+      enable = true;
+      driSupport = true;
+      extraPackages = with pkgs; [
+        amdvlk
+        vaapiVdpau
+        nvidia-vaapi-driver
+        libvdpau-va-gl
+      ];
+    };
+    brillo.enable = true;
+    bluetooth.enable = true;
+    # disable nvidia
+    nvidiaOptimus.disable = false;
+  };
+
+  users.users.root.initialPassword = "root";
+  users.users.root.shell = pkgs.bash;
 
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
     # FIXME: Replace with your username
-    your-username = {
+    "${vars.username}" = {
       # TODO: You can set an initial password for your user.
       # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
       # Be sure to change it (using passwd) after rebooting!
@@ -92,14 +221,181 @@
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["wheel"];
+      extraGroups = [ "wheel" "networkmanager" "docker" ];
+      shell = pkgs.bash;
+      packages = (with pkgs; [
+        inputs.home-manager.packages.${pkgs.system}.default
+        vscode
+        firefox
+        google-chrome
+        obs-studio
+        localsend
+        # postman
+        # jetbrains.idea-ultimate
+        # bun
+        # qq
+      ]) ++ (with pkgs.gnomeExtensions; [
+        caffeine
+        just-perfection
+]) ++ (with pkgs.python311Packages; [
+        selenium
+        black
+]);
     };
+  };
+
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = with pkgs; [ fcitx5-with-addons fcitx5-chinese-addons ];
+  };
+
+  environment.shells = with pkgs; [ bashInteractive fish ];
+
+  environment.variables = {
+    EDITOR = "helix";
+    QT_IM_MODULE = "fcitx";
+    XMODIFIERS = "@im=fcitx";
+    SDL_IM_MODULE = "fcitx";
+    GLFW_IM_MODULE = "ibus";
+  };
+
+  environment.sessionVariables = { };
+
+  environment.systemPackages = with pkgs; [
+    inputs.home-manager.packages.${pkgs.system}.default
+    gnome.adwaita-icon-theme
+    gnome.dconf-editor
+    gnome.gnome-tweaks
+    helix
+    git
+    eza
+    yazi
+    fzf
+    bat
+    fd
+    ripgrep-all
+    mcfly
+    htop
+    linux-wifi-hotspot
+    python311
+    orchis-theme
+  ];
+  # Ignored Packages
+  environment.gnome.excludePackages = (with pkgs; [ gnome-tour ])
+    ++ (with pkgs.gnome; [
+      atomix
+      epiphany
+      geary
+      gedit
+      gnome-characters
+      gnome-contacts
+      gnome-initial-setup
+      hitori
+      iagno
+      tali
+      yelp
+    ]);
+
+  documentation = {
+    enable = false;
+    doc.enable = false;
+  };
+
+  programs = {
+    command-not-found.enable = false;
+    xwayland.enable = true;
+    fish = {
+      enable = true;
+      useBabelfish = true;
+      interactiveShellInit = ''
+        set -g fish_greeting ""
+        ${pkgs.thefuck}/bin/thefuck --alias | source
+
+        # Run function to set colors that are dependant on `$term_background` and to register them so
+        # they are triggerd when the relevent event happens or variable changes.
+        set-shell-colors
+
+        # Set Fish colors that aren't dependant the `$term_background`.
+        set -g fish_color_quote        cyan      # color of commands
+        set -g fish_color_redirection  brmagenta # color of IO redirections
+        set -g fish_color_end          blue      # color of process separators like ';' and '&'
+        set -g fish_color_error        red       # color of potential errors
+        set -g fish_color_match        --reverse # color of highlighted matching parenthesis
+        set -g fish_color_search_match --background=yellow
+        set -g fish_color_selection    --reverse # color of selected text (vi mode)
+        set -g fish_color_operator     green     # color of parameter expansion operators like '*' and '~'
+        set -g fish_color_escape       red       # color of character escapes like '\n' and and '\x70'
+        set -g fish_color_cancel       red       # color of the '^C' indicator on a canceled command
+      '';
+    };
+    fzf.fuzzyCompletion = true;
+    firefox = { languagePacks = [ "zh-CN" ]; };
+    yazi.settings.yazi = { manager.show_hidden = true; };
+  };
+
+  qt = { enable = true; };
+
+  services = {
+    auto-cpufreq.enable = true;
+    btrfs.autoScrub = {
+      enable = true;
+      interval = "monthly";
+      fileSystems = [ "/" ];
+    };
+    pipewire = {
+      enable = true;
+      audio.enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      jack.enable = true;
+      pulse.enable = true;
+      wireplumber.enable = true;
+    };
+    xserver = {
+      enable = true;
+      videoDrivers = [ "amdgpu" ];
+      displayManager = {
+        gdm = { enable = true; };
+        autoLogin = {
+          enable = true;
+          user = "${vars.username}";
+        };
+      };
+      desktopManager = {
+        xterm.enable = false;
+        gnome = { enable = true; };
+      };
+      excludePackages = with pkgs; [ ];
+      libinput = {
+        enable = true;
+        mouse = { accelProfile = "adaptive"; };
+        touchpad = {
+          tapping = true;
+          naturalScrolling = true;
+          accelProfile = "adaptive";
+          disableWhileTyping = true;
+        };
+      };
+    };
+    udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+    # CUPS
+    # printing.enable = true;
+    avahi = {
+      enable = true;
+      nssmdns = true;
+      publish = {
+        enable = true;
+        addresses = true;
+        userServices = true;
+      };
+    };
+    flatpak.enable = true;
   };
 
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
-    enable = true;
+    enable = false;
     settings = {
       # Forbid root login through SSH.
       PermitRootLogin = "no";
@@ -109,5 +405,5 @@
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  system.stateVersion = "23.05";
+  system.stateVersion = "24.05";
 }
