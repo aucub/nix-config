ebee4842e5e2e3ba22217e5f55ca6b2d9bb04e33
aucub <78630225+aucub@users.noreply.github.com>
2023-12-09 02:26:23 +0800

    feat(nixos): add yazi module for file manager - yazi is a terminal-based file manager with a graphical interface - the module allows configuring various settings for yazi, such as layout, preview, opener, open rules, tasks, plugins, and input - the module also imports the yazi.nix file, which contains the actual configuration for yazi - the module is enabled by default and can be imported in the default.nix file style: format flake.nix and home.nix files - Indent nixgl input in flake.nix - Add password variable in flake.nix - Change theme in home.nix - Remove unused extensions in home.nix - Update dconf settings in home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -6,6 +6,7 @@
   lib,
   config,
   pkgs,
+  vars,
   ...
 }: {
   # You can import other home-manager modules here
@@ -16,6 +17,7 @@
     # outputs.homeManagerModules.fastfetch
     # outputs.homeManagerModules.firefox
     # outputs.homeManagerModules.chromium
+    outputs.homeManagerModules.dconf
 
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
@@ -46,6 +48,7 @@
     config = {
       # Disable if you don't want unfree packages
       allowUnfree = true;
+      allowUnfreePredicate = _: true;
     };
   };
 
@@ -203,24 +206,24 @@
       enableFishIntegration = true;
     };
     fish.functions = {
-        rga-fzf = ''
-              set RG_PREFIX 'rga --files-with-matches'
-              if test (count $argv) -gt 1
-                  set RG_PREFIX "$RG_PREFIX $argv[1..-2]"
-              end
-              set -l file $file
-              set file (
-              FZF_DEFAULT_COMMAND="$RG_PREFIX '$argv[-1]'" \
-              fzf --sort \
-                  --preview='test ! -z {} && \
-                      rga --pretty --context 5 {q} {}' \
-                  --phony -q "$argv[-1]" \
-                  --bind "change:reload:$RG_PREFIX {q}" \
-                  --preview-window='50%:wrap'
-          ) && echo "opening $file" && open "$file"
-          end
-        '';
-      };
+      rga-fzf = ''
+            set RG_PREFIX 'rga --files-with-matches'
+            if test (count $argv) -gt 1
+                set RG_PREFIX "$RG_PREFIX $argv[1..-2]"
+            end
+            set -l file $file
+            set file (
+            FZF_DEFAULT_COMMAND="$RG_PREFIX '$argv[-1]'" \
+            fzf --sort \
+                --preview='test ! -z {} && \
+                    rga --pretty --context 5 {q} {}' \
+                --phony -q "$argv[-1]" \
+                --bind "change:reload:$RG_PREFIX {q}" \
+                --preview-window='50%:wrap'
+        ) && echo "opening $file" && open "$file"
+        end
+      '';
+    };
     htop = {
       enable = true;
       settings = {
@@ -237,7 +240,7 @@
       enable = true;
       defaultEditor = true;
       settings = {
-        theme = "github_dark_colorblind";
+        theme = "fleet_dark";
         editor = {
           middle-click-paste = false;
           file-picker.hidden = false;
@@ -254,7 +257,6 @@
     };
     fzf = {
       enable = true;
-      fuzzyCompletion = true;
       defaultCommand = "rga --files || fd || find .";
       defaultOptions = [
         "--preview='bat {} -n --color=always'"
@@ -284,151 +286,17 @@
         obs-pipewire-audio-capture
         obs-scale-to-sound
         obs-vaapi
-        advanced-scene-switcher
       ];
     };
   };
-  dconf.settings = {
-        "org/gnome/shell" = {
-          favorite-apps = [
-            "org.gnome.settings.desktop"
-            "alacritty.desktop"
-            "firefox.desktop"
-            "emacs.desktop"
-            "org.gnome.nautilus.desktop"
-            "com.obsproject.studio.desktop"
-            "plexmediaplayer.desktop"
-            "smartcode-stremio.desktop"
-            "discord.desktop"
-            "steam.desktop"
-            "retroarch.desktop"
-            "com.parsecgaming.parsec.desktop"
-            "org.remmina.remmina.desktop"
-            "virt-manager.desktop"
-            # "blueman-manager.desktop"
-            # "pavucontrol.desktop"
-          ];
-              "org/gnome/nautilus/icon-view" = {
-      default-zoom-level = "standard";
+
+  services = {
+    udiskie = {
+      enable = true;
+      automount = true;
+      tray = "auto";
     };
-
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
-
-    "org/gnome/nautilus/preferences" = {
-      default-folder-viewer = "icon-view";
-      default-sort-order = "type";
-      search-filter-time-type = "last_modified";
-      search-view = "list-view";
-    };
-        "org/gnome/TextEditor" = {
-      custom-font = "FiraCode Nerd Font Mono Medium 14";
-      highlight-current-line = true;
-      indent-style = "space";
-      show-line-numbers = true;
-      show-map = true;
-      show-right-margin = true;
-      style-scheme = "builder-dark";
-      tab-width = mkUint32 4;
-      use-system-font = false;
-    };
-          disable-user-extensions = false;
-          enabled-extensions = [
-            "trayiconsreloaded@selfmade.pl"
-            "blur-my-shell@aunetx"
-            "drive-menu@gnome-shell-extensions.gcampax.github.com"
-            "dash-to-panel@jderose9.github.com"
-            "just-perfection-desktop@just-perfection"
-            "caffeine@patapon.info"
-            "clipboard-indicator@tudmotu.com"
-            "horizontal-workspace-indicator@tty2.io"
-            "bluetooth-quick-connect@bjarosze.gmail.com"
-            "battery-indicator@jgotti.org"
-            "gsconnect@andyholmes.github.io"
-            "pip-on-top@rafostar.github.com"
-            "forge@jmmaranan.com"
-            # "dash-to-dock@micxgx.gmail.com"           # Alternative Dash-to-Panel
-            # "fullscreen-avoider@noobsai.github.com"   # Dash-to-Panel Incompatable
-          ];
-        };
-
-        "org/gnome/desktop/interface" = {
-          color-scheme = "prefer-dark";
-          enable-hot-corners = false;
-          clock-show-weekday = true;
-          # gtk-theme = "adwaita-dark";
-        };
-        # "org/gnome/desktop/session" = {               # Not Working
-        #   idle-delay = "uint32 900";
-        # };
-        "org/gnome/desktop/privacy" = {
-          report-technical-problems = "false";
-        };
-        "org/gnome/desktop/calendar" = {
-          show-weekdate = true;
-        };
-        "org/gnome/desktop/wm/preferences" = {
-          action-right-click-titlebar = "toggle-maximize";
-          action-middle-click-titlebar = "minimize";
-          resize-with-right-button = true;
-          mouse-button-modifier = "<super>";
-          button-layout = ":minimize,close";
-        };
-        "org/gnome/desktop/wm/keybindings" = {
-          # maximize = ["<super>up"];                   # Floating
-          # unmaximize = ["<super>down"];
-          maximize = ["@as []"];                        # Tiling
-          unmaximize = ["@as []"];
-          switch-to-workspace-left = ["<alt>left"];
-          switch-to-workspace-right = ["<alt>right"];
-          switch-to-workspace-1 = ["<alt>1"];
-          switch-to-workspace-2 = ["<alt>2"];
-          switch-to-workspace-3 = ["<alt>3"];
-          switch-to-workspace-4 = ["<alt>4"];
-          switch-to-workspace-5 = ["<alt>5"];
-          move-to-workspace-left = ["<shift><alt>left"];
-          move-to-workspace-right = ["<shift><alt>right"];
-          move-to-workspace-1 = ["<shift><alt>1"];
-          move-to-workspace-2 = ["<shift><alt>2"];
-          move-to-workspace-3 = ["<shift><alt>3"];
-          move-to-workspace-4 = ["<shift><alt>4"];
-          move-to-workspace-5 = ["<shift><alt>5"];
-          move-to-monitor-left = ["<super><alt>left"];
-          move-to-monitor-right = ["<super><alt>right"];
-          close = ["<super>q" "<alt>f4"];
-          toggle-fullscreen = ["<super>f"];
-        };
-
-        "org/gnome/mutter" = {
-          workspaces-only-on-primary = false;
-          center-new-windows = true;
-          edge-tiling = false;                          # Tiling
-        };
-        "org/gnome/mutter/keybindings" = {
-          #toggle-tiled-left = ["<super>left"];         # Floating
-          #toggle-tiled-right = ["<super>right"];
-          toggle-tiled-left = ["@as []"];               # Tiling
-          toggle-tiled-right = ["@as []"];
-        };
-        "org/gnome/settings-daemon/plugins/power" = {
-          sleep-interactive-ac-type = "nothing";
-        };
-        "org/gnome/shell/extensions/caffeine" = {
-          enable-fullscreen = true;
-          restore-state = true;
-          show-indicator = true;
-          show-notification = false;
-        };
-      };
-
- services = {
-        udiskie = {
-          enable = true;
-          automount = true;
-          tray = "auto";
-        };
-      };
+  };
 
   # notifications about home-manager news
   news.display = "silent";
---
+++ pkgs/hiddify/default.nix
@@ -0,0 +1,54 @@
+{
+  lib,
+  stdenv,
+  appimageTools,
+  fetchzip,
+}: let
+  pname = "hiddify";
+  version = "0.11.1";
+
+  hiddify-src = stdenv.mkDerivation {
+    name = "hiddify-linux-x64.AppImage";
+
+    src = fetchzip {
+      stripRoot = false;
+      url = "https://github.com/hiddify/hiddify-next/releases/download/v${version}/hiddify-linux-x64.zip";
+      hash = "sha256-Qq9HsgIKJysmPQB5T97gj3gIcZ1/XN7GC8uq0A2Z6cA=";
+    };
+
+    installPhase = ''
+      cp hiddify-linux-x64.AppImage $out
+    '';
+  };
+
+  appimageContents = appimageTools.extractType2 {
+    inherit pname version;
+    src = hiddify-src;
+  };
+in
+  appimageTools.wrapType2 rec {
+    inherit pname version;
+    src = hiddify-src;
+
+    multiArch = false;
+    extraPkgs = appimageTools.defaultFhsEnvArgs.multiPkgs;
+
+    extraInstallCommands = ''
+      mv $out/bin/${pname}-${version} $out/bin/${pname}
+
+      install -m 444 -D ${appimageContents}/hiddify.desktop $out/share/applications/hiddify.desktop
+      install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/128x128/apps/hiddify.png $out/share/icons/hicolor/128x128/apps/hiddify.png
+      install -m 444 -D ${appimageContents}/usr/share/icons/hicolor/256x256/apps/hiddify.png $out/share/icons/hicolor/256x256/apps/hiddify.png
+      substituteInPlace $out/share/applications/hiddify.desktop \
+        --replace 'Exec=' 'Exec=env '
+    '';
+
+    meta = with lib; {
+      description = "A multi-platform client based on Sing-box that serves as a universal proxy tool-chain.";
+      homepage = "https://github.com/hiddify/hiddify-next";
+      sourceProvenance = with sourceTypes; [binaryNativeCode];
+      license = licenses.cc-by-nc-sa-40;
+      maintainers = with maintainers; [aucub];
+      platforms = ["x86_64-linux"];
+    };
+  }
