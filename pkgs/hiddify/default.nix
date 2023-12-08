1f9b90d84c2a684713775226a300294e4d0d057f
aucub <78630225+aucub@users.noreply.github.com>
2023-12-08 22:47:22 +0000

    add  hiddify

--- pkgs/hiddify/default.nix
+++ pkgs/hiddify/default.nix
@@ -1,41 +1,42 @@
 {
- lib, stdenv, appimageTools, fetchurl 
-}:
-stdenv.mkDerivation rec {
+  lib,
+  stdenv,
+  appimageTools,
+  fetchurl,
+}: let
   pname = "hiddify";
-  version = "0.10.0";
+  version = "0.11.1";
 
-  src = fetchurl {
+  srcZipped = fetchurl {
     url = "https://github.com/hiddify/hiddify-next/releases/download/v${version}/hiddify-linux-x64.zip";
-    hash = "sha256-b874ce0468570cd3e161bb1516318cbe49917bfcde18cc117a93d411cc623578";
+    hash = "";
   };
 
-  appimageContents = appimageTools.extract { inherit pname version src.extract.hiddify-linux-x64.AppImage; };
-
-  installPhase = appimageTools.wrapType2 rec {
-    inherit pname version src meta;
-
-    extraPkgs = p: [ p.ayatana-ido p.libayatana-appindicator p.libayatana-indicator p.libdbusmenu p.libepoxy ];
+  appimageContents = appimageTools.extractType2 {
+    inherit pname version;
+    src = "hiddify-linux-x64.AppImage";
+  };
+in
+  appimageTools.wrapType2 rec {
+    inherit pname version;
+    src = "hiddify-linux-x64.AppImage";
 
     extraInstallCommands = ''
       mv $out/bin/${pname}-${version} $out/bin/${pname}
 
-      install -m 444 -D ${appimageContents}/org.localsend.localsend_app.desktop \
-        $out/share/applications/${pname}.desktop
-      substituteInPlace $out/share/applications/${pname}.desktop \
-        --replace 'Exec=localsend_app' "Exec=$out/bin/localsend"
-
-      install -m 444 -D ${appimageContents}/localsend.png \
-        $out/share/icons/hicolor/256x256/apps/localsend.png
+      install -m 444 -D ${appimageContents}/hiddify.desktop $out/share/applications/hiddify.desktop
+      install -m 444 -D ${appimageContents}/hiddify.png $out/share/icons/hicolor/128x128/apps/hiddify.png
+      install -m 444 -D ${appimageContents}/hiddify.png $out/share/icons/hicolor/256x256/apps/hiddify.png
+      substituteInPlace $out/share/applications/hiddify.desktop \
+        --replace 'Exec=' 'Exec=env '
     '';
-  };
 
-  meta = with lib; {
-    description = "A multi-platform client based on Sing-box that serves as a universal proxy tool-chain.";
-    homepage = "https://github.com/hiddify/hiddify-next";
-    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
-    license = licenses.cc-by-nc-sa-40;
-    maintainers = with maintainers; [ aucub ];
-    platforms = [ "x86_64-linux" ];
-  };
-}
+    meta = with lib; {
+      description = "A multi-platform client based on Sing-box that serves as a universal proxy tool-chain.";
+      homepage = "https://github.com/hiddify/hiddify-next";
+      sourceProvenance = with sourceTypes; [binaryNativeCode];
+      license = licenses.cc-by-nc-sa-40;
+      maintainers = with maintainers; [aucub];
+      platforms = ["x86_64-linux"];
+    };
+  }
