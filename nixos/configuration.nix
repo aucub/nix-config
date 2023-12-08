1edf5995ad208e0c8fa16a817d030059537e5df5
aucub <78630225+aucub@users.noreply.github.com>
2023-12-09 02:26:23 +0800

    feat(nixos): add yazi module for file manager - yazi is a terminal-based file manager with a graphical interface - the module allows configuring various settings for yazi, such as layout, preview, opener, open rules, tasks, plugins, and input - the module also imports the yazi.nix file, which contains the actual configuration for yazi - the module is enabled by default and can be imported in the default.nix file style: format flake.nix and home.nix files - Indent nixgl input in flake.nix - Add password variable in flake.nix - Change theme in home.nix - Remove unused extensions in home.nix - Update dconf settings in home.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,10 +1,19 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
-{ inputs, outputs, lib, config, pkgs, vars, ... }: {
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  vars,
+  ...
+}: {
   # You can import other NixOS modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/nixos):
     # outputs.nixosModules.example
+    outputs.nixosModules.yazi
 
     # Or modules from other flakes (such as nixos-hardware):
     # inputs.hardware.nixosModules.common-cpu-amd
@@ -18,8 +27,16 @@
     # Import your generated (nixos-generate-config) hardware configuration
     ./hardware-configuration.nix
     # Import home-manager's NixOS module
-    # inputs.home-manager.nixosModules.home-manager
+    inputs.home-manager.nixosModules.home-manager
   ];
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs vars outputs; };
+    users = {
+      # Import your home-manager configuration
+      ${vars.username} = import ../home-manager/home.nix;
+    };
+  };
 
   nixpkgs = {
     # You can add overlays here
@@ -48,16 +65,19 @@
 
   # This will add each flake input as a registry
   # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: { inherit flake; }))
+  nix.registry =
+    (lib.mapAttrs (_: flake: {inherit flake;}))
     ((lib.filterAttrs (_: lib.isType "flake")) inputs);
 
   # This will additionally add your inputs to the system's legacy channels
   # Making legacy nix commands consistent as well, awesome!
-  nix.nixPath = [ "/etc/nix/path" ];
-  environment.etc = lib.mapAttrs' (name: value: {
-    name = "nix/path/${name}";
-    value.source = value.flake;
-  }) config.nix.registry;
+  nix.nixPath = ["/etc/nix/path"];
+  environment.etc =
+    lib.mapAttrs' (name: value: {
+      name = "nix/path/${name}";
+      value.source = value.flake;
+    })
+    config.nix.registry;
 
   nix.settings = {
     # Deduplicate and optimize nix store
@@ -65,6 +85,7 @@
     substituters = [
       "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://mirrors.bfsu.edu.cn/nix-channels/store"
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://cache.nixos.org/"
       "https://qihaiumi.cachix.org"
       "https://nix-community.cachix.org"
@@ -75,13 +96,13 @@
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
     ];
-    trusted-users = [ "root" "@wheel" ];
+    trusted-users = ["root" "@wheel"];
   };
 
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 2d";
+    options = "--delete-older-than 7d";
   };
   # FIXME: Add the rest of your current configuration
 
@@ -112,7 +133,7 @@
       "NVreg_PreserveVideoMemoryAllocations=1"
     ];
     consoleLogLevel = 3;
-    initrd.kernelModules = [ "btrfs" ];
+    initrd.kernelModules = ["btrfs"];
     kernelModules = [
       "v4l2loopback"
       "bbswitch"
@@ -137,7 +158,7 @@
   time.timeZone = "Asia/Shanghai";
 
   i18n.defaultLocale = "en_US.UTF-8";
-  i18n.supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
+  i18n.supportedLocales = ["zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8"];
 
   fonts = {
     enableDefaultPackages = true;
@@ -158,10 +179,10 @@
     fontconfig = {
       enable = true;
       defaultFonts = {
-        serif = [ "Noto Serif CJK SC" ];
-        sansSerif = [ "Sarasa UI SC" ];
-        monospace = [ "Sarasa Mono SC" ];
-        emoji = [ "Twemoji" "Noto Color Emoji" ];
+        serif = ["Noto Serif CJK SC"];
+        sansSerif = ["Sarasa UI SC"];
+        monospace = ["Sarasa Mono SC"];
+        emoji = ["Twemoji" "Noto Color Emoji"];
       };
     };
   };
@@ -169,12 +190,15 @@
   xdg.portal = {
     enable = true;
     xdgOpenUsePortal = true;
-    extraPortals = with pkgs; [ ];
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+    ];
   };
 
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
+
   hardware = {
+    pulseaudio.enable = false;
     nvidia = {
       package = pkgs.linuxKernel.packages.linux_lqx.nvidia_x11_vulkan_beta_open;
       powerManagement.enable = true;
@@ -205,8 +229,13 @@
     nvidiaOptimus.disable = false;
   };
 
+  zramSwap = {
+    enable = true;
+    memoryPercent = 25;
+  };
+
   users.users.root.initialPassword = "root";
-  users.users.root.shell = pkgs.bash;
+  users.users.root.shell = pkgs.bashInteractive;
 
   # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
@@ -215,41 +244,58 @@
       # TODO: You can set an initial password for your user.
       # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
       # Be sure to change it (using passwd) after rebooting!
-      initialPassword = "correcthorsebatterystaple";
+      initialPassword = "${vars.password}";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
         # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
       ];
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = [ "wheel" "networkmanager" "docker" ];
-      shell = pkgs.bash;
-      packages = (with pkgs; [
-        inputs.home-manager.packages.${pkgs.system}.default
-        vscode
-        firefox
-        google-chrome
-        obs-studio
-        localsend
-        # postman
-        # jetbrains.idea-ultimate
-        # bun
-        # qq
-      ]) ++ (with pkgs.gnomeExtensions; [
-        caffeine
-        just-perfection
-]) ++ (with pkgs.python311Packages; [
-        selenium
-        black
-]);
+      extraGroups = ["wheel" "networkmanager" "docker"];
+      shell = pkgs.bashInteractive;
+      packages =
+        (with pkgs; [
+          inputs.home-manager.packages.${pkgs.system}.default
+        ])
+        ++ 
+        (with pkgs; [
+          vscode
+          firefox
+          google-chrome
+          chromedriver
+          impression
+          obs-studio
+          celluloid
+          localsend
+          gopeed
+          orchis-theme
+          typst
+          # jetbrains.idea-ultimate
+          # jdk21
+          # bun
+          # qq
+        ])
+        ++ (with pkgs.gnomeExtensions; [
+          appindicator
+          kimpanel
+          caffeine
+        ])
+        ++ (with pkgs.python311Packages; [
+          selenium
+          black
+        ])
+        ++ 
+        (with pkgs; [
+          npm-check-updates
+        ]);
     };
   };
 
   i18n.inputMethod = {
     enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [ fcitx5-with-addons fcitx5-chinese-addons ];
+    fcitx5.addons = with pkgs; [fcitx5-with-addons fcitx5-chinese-addons];
   };
 
-  environment.shells = with pkgs; [ bashInteractive fish ];
+  environment.shells = with pkgs; [bashInteractive fish];
 
   environment.variables = {
     EDITOR = "helix";
@@ -259,13 +305,20 @@
     GLFW_IM_MODULE = "ibus";
   };
 
-  environment.sessionVariables = { };
+  environment.systemPackages =
+  (with pkgs; [
+          inputs.home-manager.packages.${pkgs.system}.default
+        ])
+        ++
+         (with pkgs.gnome; [
+    adwaita-icon-theme
+    dconf-editor
+    gnome-tweaks
+        ])
+        ++
+   (with pkgs; [
 
-  environment.systemPackages = with pkgs; [
-    inputs.home-manager.packages.${pkgs.system}.default
-    gnome.adwaita-icon-theme
-    gnome.dconf-editor
-    gnome.gnome-tweaks
+    difftastic
     helix
     git
     eza
@@ -275,30 +328,46 @@
     fd
     ripgrep-all
     mcfly
+    tlrc
     htop
     linux-wifi-hotspot
     python311
     orchis-theme
-  ];
-  # Ignored Packages
-  environment.gnome.excludePackages = (with pkgs; [ gnome-tour ])
+  ]);
+  # GNOME Ignored Packages
+  environment.gnome.excludePackages =
+    (with pkgs; [
+      gnome-tour
+      gnome-photos
+      ])
     ++ (with pkgs.gnome; [
-      atomix
-      epiphany
-      geary
-      gedit
-      gnome-characters
       gnome-contacts
       gnome-initial-setup
-      hitori
-      iagno
-      tali
       yelp
+        cheese # webcam tool
+  gnome-music
+  gnome-terminal
+  gedit # text editor
+  epiphany # web browser
+  geary # email reader
+  evince # document viewer
+  gnome-characters
+  totem # video player
+  tali # poker game
+  iagno # go game
+  hitori # sudoku game
+  atomix # puzzle game
     ]);
 
   documentation = {
     enable = false;
+    nixos.enable = false;
+    man.enable = false;
+    man.mandoc.enable = false;
+    man.man-db.enable = false;
     doc.enable = false;
+    info.enable = false;
+    dev.enable = false;
   };
 
   programs = {
@@ -329,18 +398,24 @@
       '';
     };
     fzf.fuzzyCompletion = true;
-    firefox = { languagePacks = [ "zh-CN" ]; };
-    yazi.settings.yazi = { manager.show_hidden = true; };
+    firefox = {
+      enable = true;
+      languagePacks = ["zh-CN"];
+    };
   };
 
-  qt = { enable = true; };
+  qt = {
+    enable = true;
+    style = "adwaita";
+    platformTheme = "qt5ct";
+  };
 
   services = {
     auto-cpufreq.enable = true;
     btrfs.autoScrub = {
       enable = true;
       interval = "monthly";
-      fileSystems = [ "/" ];
+      fileSystems = ["/"];
     };
     pipewire = {
       enable = true;
@@ -353,9 +428,9 @@
     };
     xserver = {
       enable = true;
-      videoDrivers = [ "amdgpu" ];
+      videoDrivers = ["amdgpu" "nvidia"];
       displayManager = {
-        gdm = { enable = true; };
+        gdm = {enable = true;};
         autoLogin = {
           enable = true;
           user = "${vars.username}";
@@ -363,12 +438,12 @@
       };
       desktopManager = {
         xterm.enable = false;
-        gnome = { enable = true; };
+        gnome = {enable = true;};
       };
-      excludePackages = with pkgs; [ ];
+      excludePackages = with pkgs; [ xterm ];
       libinput = {
         enable = true;
-        mouse = { accelProfile = "adaptive"; };
+        mouse = {accelProfile = "adaptive";};
         touchpad = {
           tapping = true;
           naturalScrolling = true;
@@ -377,18 +452,9 @@
         };
       };
     };
-    udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
+    udev.packages = with pkgs; [gnome.gnome-settings-daemon];
     # CUPS
     # printing.enable = true;
-    avahi = {
-      enable = true;
-      nssmdns = true;
-      publish = {
-        enable = true;
-        addresses = true;
-        userServices = true;
-      };
-    };
     flatpak.enable = true;
   };
 
--- pkgs/gopeed/default.nix
+++ pkgs/gopeed/default.nix
@@ -1,54 +1,53 @@
-{
-  dpkg,
-  fetchurl,
-  glibc,
-  gtk3,
-  lib,
-  libayatana-appindicator,
-  stdenv,
-  at-spi2-core,
-  cairo,
-  harfbuzz,
-  pango,
-  libepoxy,
-  libdbusmenu,
-  wrapGAppsHook,
-  udev,
-  autoPatchelfHook,
-  libayatana-indicator,
-  ayatana-ido,
-}:
+{ dpkg, fetchurl, glibc, gtk3, lib, libayatana-appindicator, stdenv, at-spi2-core, cairo, harfbuzz, pango, libepoxy, libdbusmenu, wrapGAppsHook, makeWrapper, udev, autoPatchelfHook, libayatana-indicator, ayatana-ido }:
+
 stdenv.mkDerivation rec {
   pname = "gopeed";
-  version = "1.3.12";
+  version = "1.5.1";
 
   src = fetchurl {
     url = "https://github.com/GopeedLab/gopeed/releases/download/v${version}/Gopeed-v${version}-linux-amd64.deb";
     hash = "sha256-XwB3+NtjeCqg3vHit4FRRpfNA65afH1Yc0QjIananqw=";
   };
 
-  nativeBuildInputs = [dpkg];
+  nativeBuildInputs = [    
+    autoPatchelfHook
+    (wrapGAppsHook.override { inherit makeWrapper; })
+    dpkg
+];
 
-  buildInputs = [wrapGAppsHook];
-
-  unpackCmd = "dpkg-deb -x $curSrc source";
+  runtimeDependencies = [     
+    at-spi2-core
+    gtk3
+    cairo
+    harfbuzz
+    pango
+    libepoxy
+    libayatana-appindicator
+    libdbusmenu
+    libayatana-indicator
+    ayatana-ido 
+    ];
 
   installPhase = ''
     runHook preInstall
 
-      mv usr $out
-      substituteInPlace $out/share/applications/gopeed.desktop \
-        --replace "/usr" $out
+mkdir -p $out/bin
+cp -r opt $out/opt
+cp -r usr/share $out/share
+    substituteInPlace $out/share/applications/gopeed.desktop \
+      --replace "/opt/gopeed/gopeed" "$out/bin/gopeed" \
+      --replace "/usr/share" "$out/share"
+    ln -s $out/opt/gopeed/gopeed $out/gopeed/gopeed
 
-      runHook postInstall
+    runHook postInstall
   '';
 
   meta = with lib; {
-    description = "Gopeed (full name Go Speed), a high-speed downloader developed by Golang + Flutter, supports (HTTP, BitTorrent, Magnet) protocol, and supports all platforms.";
-    homepage = "https://github.com/GopeedLab/gopeed";
+    homepage = "https://gopeed.com";
+    description = "A modern download manager that supports all platforms. Built with Golang and Flutter.";
     license = licenses.gpl3;
-    platforms = lib.platforms.linux;
-    sourceProvenance = [lib.sourceTypes.binaryNativeCode];
-    maintainers = with maintainers; [aucub];
+    platforms = [ "x86_64-linux" ];
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+    maintainers = with lib.maintainers; [ aucub ];
   };
 }
